3b033d2b247609dbb84a09591cd685e35e3ed200
rtoddMMU <rtodd@mtmercy.edu>
2024-06-04 08:56:26 -0500

    Updated concatenate_dly.bash,read_split_dly_files.R, snakefile: completed!

--- code/concatenate_dly.bash
+++ code/concatenate_dly.bash
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+mkdir -p data/temp
+tar Oxvzf data/ghcnd_all.tar.gz | grep "PRCP" | split -l 1000000 --filter 'gzip > data/temp/$FILE.gz' 
 
-tar Oxvzf data/ghcnd_all.tar.gz | grep "PRCP" | gzip > data/ghcnd_cat.gz
+code/read_split_dly_files.R 
+
+rm -rf data/temp
